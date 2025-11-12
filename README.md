58f0723eeb14f17132f69a697168e1b818e21d63
jorge soberanis gonzález <jorgesoberanis174@gmail.com>
2025-11-12 13:16:31 -0600

    Estructura

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dentista
 Página backend de un consultorio 
+En src va la lógica y en db la conexión a la base de datos
---
+++ db/conexion.php
@@ -0,0 +1,24 @@
+<?php
+$host = 'localhost';
+$db   = 'dentista';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+// Data Source Name (DSN) indica el tipo de DB (mysql, pgsql, etc.)
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Conexión a la base de datos exitosa.";
+
+} catch (\PDOException $e) {
+     // Mostrar el error de conexión
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
